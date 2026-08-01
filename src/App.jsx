3eef8aef9de6d1fc197bb042298f9439d7f899ec
HESBOYALNE <4gs6776@gmail.com>
2026-08-01 23:05:52 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
-hh
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
+import Signup from './pages/auth/Signup'
+import Login from './pages/auth/Login'
+import Dashboard from './pages/dashboard/Dashboard'
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signup" replace />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={
+            <ProtectedRoute><Dashboard /></ProtectedRoute>
+          } />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  )
+}
