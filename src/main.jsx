9aa36a04d126c2aca35505b5fbf46c8ca246fe03
HESBOYALNE <4gs6776@gmail.com>
2026-08-01 23:04:31 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
-hh
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './theme/globals.css'
+import './theme/components.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
